12331b9467fd62a37edd7333beadd2ea3bb52991
Inoel Rivera <jimadoriicrafts@gmail.com>
2016-06-20 15:41:49 -0700

    All assignment1 files added

---
+++ js/script.js
@@ -0,0 +1,179 @@
+/* problem 1
+var someMonth;
+function theMonth 		// function to return current month
+current-month 		// a constant stays the same
+var summerMonth; 		// an array of summer months
+MyLibraryAfunction 	// a function 
+*/
+
+
+/* problem 2
+//numeric literal expression below
+5.5;
+//string literal expression below
+"hello"
+//boolean literal expressions below
+true
+//null literal expression below
+null
+*/
+
+/* problem 3
+var aProblem = 2 + (1 * 5) - 3;
+var aPhrase = phrase1 + "radius" + phrase2;
+*/
+
+/* problem 4
+//Hungarian notation below
+var strFirstName = "";
+var strLastName = "";
+var strAddress = "";
+var strCity = "";
+var strState = "";
+//Camel casing below
+var zipCode = "";
+var yourAge = "";
+var referralSource = "";
+var mayWeContactYou = "";
+*/
+
+/* problem 5
+var strFirstName = "John";
+var strLastName = "Doe";
+var strAddress = null;
+var strCity = "San Diego";
+var strState = "California";
+var zipCode = 92154;
+var yourAge = 21;
+var referralSource = true;
+var mayWeContactYou = false;
+*/
+
+/* problem 6
+//first part
+var numberAndString = 24 + " Well hello there.";
+console.log(numberAndString);
+
+//second part
+var boolAndString = false + " Well hello there.";
+console.log(boolAndString);
+
+//third part
+var numberAndBool = false + "" + 24;
+console.log(numberAndBool);
+*/
+
+/* problem 7
+window.alert(firstName);
+var firstName;
+firstName = "John";
+*/
+
+/* problem 8
+//Backslash for single quote for I'm is not used: INVALID
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+console.log(someString);
+*/
+
+/* problem 9
+var firstName = null;
+console.log(firstName);
+console.log(lastName);
+*/
+
+/* problem 10
+var firstName = "John";
+console.log(typeof firstName);
+var x = 10;
+console.log(typeof x);
+var truth = true;
+console.log(typeof truth);
+var car = {type:"Fiat", model:"500", color:"white"};
+console.log(typeof car);
+var foot
+console.log(typeof foot);
+*/
+
+/* problem 11
+var myName = "Inoel Rivera";
+window.alert("Hello " + myName + ", welcome to the JavaScript class!");
+*/
+
+/* problem 12
+var name ="Inoel";
+
+var myName = name;
+window.alert("Hello " + myName + ", welcome to the JavaScript class!");
+*/
+
+/* problem 13
+var name ="Inoel";
+var myName = name;
+var course = "JavaScript";
+window.alert("Hello " + myName + ", welcome to the " + course + " class!");
+*/
+
+/* problem 14
+var name ="Inoel Rivera.";
+var myName = name;
+var course = "JavaScript";
+window.alert("Hello " +  myName + "\n" + "welcome to the " + course + " class!");
+*/
+
+/* problem 15
+var name = prompt("Hello! Please enter your name" , "Type here");
+var myName = name;
+var course = "JavaScript";
+window.prompt("Hello " +  myName + ",\n" + "welcome to the " + course + " class!");
+*/
+
+/* problem 16
+var name = prompt("Hello! Please enter your name" , "Type here");
+var myName = name;
+var course = prompt ("Which course will you be taking?");
+window.prompt("Hello " + myName + ",\n" + "welcome to the " + course + " class!");
+*/
+
+/* problem 17
+var x = 10;
+var y = 20;
+console.log(x + y);
+*/
+
+/* problem 18
+var x = 20;
+console.log(x + 20);
+*/
+
+/* problem 19
+var x = 20;
+console.log(x * 5);
+*/
+
+/* problem 20
+var x = 20 % 3;
+console.log(x / 1);
+*/
+
+/* problem 21
+console.log(5 == 5);
+*/
+
+/* problem 22
+console.log(3 > 6);
+*/
+
+/* problem 23
+var widget = [];
+console.log(typeof widget);
+*/
+
+/* problem 24
+var widget = [];
+console.log(widget instanceof Object);
+*/
+
+/* problem 25
+var widget = [];
+console.log(widget instanceof String);
+*/
